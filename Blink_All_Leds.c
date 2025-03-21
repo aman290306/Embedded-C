ca19fe5e986e474fcfaab6e232dbd6fec50da81c
aman290306 <29amanappini55555@gmail.com>
2025-03-22 01:26:28 +0530

    Update and rename Blinking all LEDS to Blink_All_Leds.c

---
+++ Blink_All_Leds.c
@@ -0,0 +1,18 @@
+#include<LPC17xx.h>
+unsigned int i,j;
+int main(){
+  SystemInit();
+  SystemCoreClockUpdate();
+//I'm going to use 0.4-0.11 as output pins.
+  LPC_PINCON->PINSEL0=0x0; //Set 0.4-0.11 as GPIO
+  LPC_GPIO0->FIODIR=0xFF0;  //Set 0.4-0.11 as output pins
+while(1){
+  for(i=0;i<10;i++){
+    LPC_GPIO0->FIOSET=0xFF0; //Set 0.4-0.11 pins high
+    for(j=0;j<50000;j++); //delay
+    LPC_GPIO0->FIOCLR=0xFF0; //Set 0.4-0.11 pins Low
+    for(j=0;j<50000;j++); //delay
+  }
+  }
+}
+//Output: all 8LEDS should blink 10times
